38cff6cf8302d4cb9611e9d64a848f2cfd28ecbe
Jose Torres <asapnomad@gmail.com>
2022-04-12 08:17:42 -0700

    Removed comment.

--- pages/projects.js
+++ pages/projects.js
@@ -4,52 +4,41 @@ import ProjectComponent from "../components/ProjectComponent";
 
 const Projects = () => {
   return (
-      <Box p="2rem">
-<Heading pb="1rem" size="3xl">
-          Projects
+    <Box p="2rem">
+      <Heading pb="1rem" size="3xl">
+        Projects
       </Heading>
-    <Grid
-      templateColumns={[
-        "repeat(1, 1fr)",
-        "repeat(1, 1fr)",
-        "repeat(1, 1fr)",
-        "repeat(1, 1fr)",
-        "repeat(2, 1fr)",
-      ]}
-
-    >
-      <ProjectComponent
-        projectImage={"https://www.vidoozles.com/1.png"}
-        projectTitle={"Vidoozles"}
-        projectLink={"https://www.vidoozles.com"}
-        projectDescription={
-          "Users can exchange $MATIC for a Vidoozle ERC-721 token. ERC-721 tokens are burnable and redeemable for $EMBER. Art, and code written by me. This is a developing project. Released in Febrary, 2022."
-        }
-      />
-      <ProjectComponent
-        projectImage={
-          "https://lh3.googleusercontent.com/rd0cWZr19zFY2u3fdMrCwuwBP0eOeIqLeOfdIKWOtZS6ZPo-iTfG67zr94yMK8bfB-2yOuRrhnn5R7fpHf85dpAniejsVpw_j2LKyWo=s0"
-        }
-        projectTitle={"Party Pumpkins"}
-        projectLink={"http://partypumpkins.io"}
-        projectDescription={
-          "Users can exchange $MATIC for a Party Pumpkin ERC-721 token. Art, and code written by me. Released October 1, 2021."
-        }
-      />
-      {/* <ProjectComponent
-        projectImage={
-          "https://lh3.googleusercontent.com/rd0cWZr19zFY2u3fdMrCwuwBP0eOeIqLeOfdIKWOtZS6ZPo-iTfG67zr94yMK8bfB-2yOuRrhnn5R7fpHf85dpAniejsVpw_j2LKyWo=s0"
-        }
-        projectTitle={"Party Pumpkins"}
-        projectLink={"http://partypumpkins.io"}
-        projectDescription={
-          "Users can exchange $MATIC for a Party Pumpkin ERC-721 token. Art, and code written by me. Released October 1, 2021."
-        }
-      /> */}
-   
-    </Grid>
-      </Box>
-      
+      <Grid
+        templateColumns={[
+          "repeat(1, 1fr)",
+          "repeat(1, 1fr)",
+          "repeat(1, 1fr)",
+          "repeat(1, 1fr)",
+          "repeat(2, 1fr)",
+        ]}
+        gap="5"
+      >
+        <ProjectComponent
+          projectImage={"https://www.vidoozles.com/1.png"}
+          projectTitle={"Vidoozles"}
+          projectLink={"https://www.vidoozles.com"}
+          projectDescription={
+            "Users can exchange $MATIC for a Vidoozle ERC-721 token. ERC-721 tokens are burnable and redeemable for $EMBER. Art, and code written by me. This is a developing project. Released in Febrary, 2022."
+          }
+        />
+        <ProjectComponent
+          projectImage={
+            "https://lh3.googleusercontent.com/rd0cWZr19zFY2u3fdMrCwuwBP0eOeIqLeOfdIKWOtZS6ZPo-iTfG67zr94yMK8bfB-2yOuRrhnn5R7fpHf85dpAniejsVpw_j2LKyWo=s0"
+          }
+          projectTitle={"Party Pumpkins"}
+          projectLink={"http://partypumpkins.io"}
+          projectDescription={
+            "Users can exchange $MATIC for a Party Pumpkin ERC-721 token. Art, and code written by me. Released October 1, 2021."
+          }
+        />
+       
+      </Grid>
+    </Box>
   );
 };
 
